41c8e6ce164a1f0acfd3d89d0a12ba70fd28e694
Guilherme Alexandre Sant'Ana <guilherme.devel@gmail.com>
2022-07-12 16:54:28 -0300

    Adjusts for Publishing and Bump Version to 0.0.7

--- build.sh
+++ build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+./gradlew tuna:clean &&
+./gradlew tuna-android:clean &&
+./gradlew tuna-android-kt:clean &&
+./gradlew tuna-google-pay:clean &&
+./gradlew tuna-java:clean &&
+./gradlew tuna-wallets:clean &&
+./gradlew tunacommons:clean &&
+./gradlew tunacr:clean &&
+./gradlew tunakt:clean &&
+./gradlew tunaui:clean &&
+
+./gradlew tuna:assemble &&
+./gradlew tuna-android:assemble &&
+./gradlew tuna-android-kt:assemble &&
+./gradlew tuna-google-pay:assemble &&
+./gradlew tuna-java:assemble &&
+./gradlew tuna-wallets:assemble &&
+./gradlew tunacommons:assemble &&
+./gradlew tunacr:assemble &&
+./gradlew tunakt:assemble &&
+./gradlew tunaui:assemble &&
+
 ./gradlew tuna:publishReleasePublicationToSonatypeRepository &&
 ./gradlew tuna-android:publishReleasePublicationToSonatypeRepository &&
 ./gradlew tuna-android-kt:publishReleasePublicationToSonatypeRepository &&
